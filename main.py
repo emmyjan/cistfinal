5f808d7febebb78f8e0654f9c62afc7cf770bcff
sphocto <sphocto355@gmail.com>
2026-04-27 16:11:51 -0500

    test

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     print(wb)
     wb.place_stone(2, 2, name="Bobbiam")
     wb.place_stone(2, 1, name="Robbiam")
+    # test that maddy can exist
 
     print(wb.get_group_liberties(2,2))
     print(wb)
