0d01e2ac65182f8ef260afacee3b2021cf997a83
emmyjan <emily.janssen.x@gmail.com>
2026-05-01 16:19:13 -0500

    Refractored drawing.py

--- drawing.py
+++ drawing.py
@@ -12,28 +12,34 @@ class Drawer:
         self.clock = pygame.time.Clock()
         self.board = board
         self.__bg = pygame.image.load("board.png")
+        self.set_board_params( (228, 25), (760, 569))
         pygame.init()
+
+    def set_board_params(self, start_coord: tuple, end_coord: tuple):
+        self.start_coord = start_coord
+        self.end_coord = end_coord
 
     def draw_update(self) -> None:
         self.screen.blit(self.__bg, (0, 0))
-        self.draw_board(self.board, (228, 25), (760, 569))
+        self.draw_board()
+        self.draw_highlight()
         pygame.display.flip()
         self.clock.tick(60)
 
-    def draw_board(self, board: board.Board, start_coord: tuple, end_coord: tuple) -> None:
+    def draw_board(self) -> None:
         """Draws stones from board's world list.\n
         Requires start and end coord of in-game display board"""
-        current_coord = list(start_coord[:])
-        board_ptr = board.get_board() #Create a reference to the world board
+        current_coord = list(self.start_coord[:])
+        board_ptr = self.board.get_board() #Create a reference to the world board
         MAGIC_NUMBER = 1.25
 
         size = len(board_ptr)
-        space_size = (end_coord[0] - start_coord[0]) // size # Get distance between the intersections  
+        space_size = (self.end_coord[0] - self.start_coord[0]) // size # Get distance between the intersections  
         for pos_y, y_dim in enumerate(board_ptr):
             for pos_x, x_dim in enumerate(y_dim):
                 color = ""
                 stone_color = board_ptr[pos_y][pos_x].getColor()
-                current_coord = [start_coord[0] + pos_x * space_size *MAGIC_NUMBER, start_coord[1] + pos_y * space_size *MAGIC_NUMBER]
+                current_coord = [self.start_coord[0] + pos_x * space_size *MAGIC_NUMBER, self.start_coord[1] + pos_y * space_size *MAGIC_NUMBER]
                 if stone_color == stone.Stone.COLOR_EMPTY: #Determine cirlce color
                     continue
                 elif stone_color == stone.Stone.COLOR_WHITE:
@@ -42,3 +48,7 @@ class Drawer:
                     color = "black" 
 
                 pygame.draw.circle(self.screen, color, current_coord, 25)
+
+    def draw_highlight(self) -> None:
+        mouse_pos = pygame.mouse.get_pos()
+        print(f"{mouse_pos}")
